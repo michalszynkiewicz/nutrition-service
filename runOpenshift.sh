5de4708789c6e2b934feca3cf2ddb0adcc3ee75a
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2017-11-07 19:45:16 +0100

    fixed checking openshift project name

--- runOpenshift.sh
+++ runOpenshift.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i s/MINISHIFT_IP/$(minishift ip)/g src/main/resources/project-stages.yml
-sed -i s/MINISHIFT_PROJECT/$(oc project)/g src/main/resources/project-stages.yml
+sed -i "s/MINISHIFT_IP/$(minishift ip)/g" src/main/resources/project-stages.yml
+sed -i "s/MINISHIFT_PROJECT/$(oc project -q)/g" src/main/resources/project-stages.yml
 
 mvn clean fabric8:deploy -Dopenshift
